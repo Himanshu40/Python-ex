9dfb8ee02acdf913f918b55cb20932034eaaa1b0
Himanshu Nayak <himanshuwindows8.1@gmail.com>
2018-05-25 11:44:44 +0530

    Support of Python 3

--- Talkingthetalk/python1.py
+++ Talkingthetalk/python1.py
@@ -11,15 +11,15 @@ def div(num1=1,num2=1):
     return (num1/num2)
 
 def hello(argcount, *varargs):
-    print"You passed ",argcount," arguments"
+    print("You passed ",argcount," arguments")
     for arg in varargs:
-        print arg
+        print (arg)
 
-print("Hello Calculator\n")
-num1=int(input("Enter a number: \n"))
-num2=int(input("Enter another number: \n"))
-print num1," + ",num2," = ",sum(num1,num2)
-print num1," - ",num2," = ",sub(num1,num2)
-print num1," * ",num2," = ",mul(num1,num2)
-print num1," / ",num2," = ",div(num1,num2)
+print("Hello Calculator")
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+print (num1," + ",num2," = ",sum(num1,num2))
+print (num1," - ",num2," = ",sub(num1,num2))
+print (num1," * ",num2," = ",mul(num1,num2))
+print (num1," / ",num2," = ",div(num1,num2))
 hello(4,3,8,9,10)
