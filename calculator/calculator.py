91d12358d260a6c0dedf787f7aa502c355cb7248
Himanshu Nayak <himanshuwindows8.1@gmail.com>
2018-06-07 15:02:43 +0530

    Calculator program

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,2 +1,30 @@
-print("Hi ! I am your basic calculator")
-print("Enter a number:-")
+import sys
+
+print('Hey, I am your basic calculator :D')
+while True:
+    num1 = int(input('Enter a number:- '))
+    num2 = int(input('Enter another number:- '))
+    op = input('Which operation do you need? +,-,/,*,// ')
+    if op == '+':
+        calc = num1 + num2
+    elif op == '-':
+        calc = num1 - num2
+    elif op == '*':
+        calc = num1 * num2
+    elif op == '/':
+        calc = num1 / num2
+    elif op == '//':
+        calc = num1 // num2
+    else:
+        print('Enter any valid operation')
+        opt = input('Do you want to retry? Yes or No ')
+        if opt == 'Yes':
+            continue
+        else:
+            sys.exit()
+    print(str(num1) + op + str(num2) + '=' + str(calc))
+    opt = input('Any other calculation? Yes or No ')
+    if opt == 'Yes':
+        continue
+    else:
+        sys.exit()
